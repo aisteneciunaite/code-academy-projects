75bb837a59e3474f850f4c274a1822e8d2c94684
Aiste Neciunaite <a.neciunaite@gmail.com>
2019-12-08 22:51:51 +0200

    Upgr slide-to-view, re-made mobile menu w JS

--- CA_Exam/app/script.js
+++ CA_Exam/app/script.js
@@ -1,73 +1,73 @@
-/*Scroll section into view*/ 
 
 var arr = document.querySelectorAll('body > header > div > nav > a');
+var menuToggleButton = document.getElementById('menu-button');
+var topMenu = document.getElementById('menu');
+var formToggleButton = document.getElementById('sub-form-tgl');
+var subscriptionForm = document.getElementById('subscription-form');
+var submitButtons = document.querySelectorAll('input[type="button"]');
+var textInputs = document.querySelectorAll('input[type="text"], input[type="tel"]');
+var radioInputs = document.querySelectorAll('input[type="radio"]');
+
+/*Scroll section into view*/ 
 
 for (let index = 0; index < arr.length; index++) {
     arr[index].addEventListener('click', function() {
-        document.getElementById(this.getAttribute('data-target')).scrollIntoView({behavior: "smooth"});
-        event.preventDefault();
+      el = document.getElementById(this.getAttribute('data-target'));
+      var topOfElement = el.offsetTop;
+      window.scroll({ top: topOfElement, behavior: "smooth" });
+      event.preventDefault();
+
+      if (topMenu.classList.contains('open')) {
+        topMenu.classList.remove('open');
+        menuToggleButton.classList.remove('open');
+      }
     })    
 }
-
 /* Initialize reviews swiper */
 
   var swiper = new Swiper('.rw-swiper', {
     slidesPerView: 1,
     breakpoints: {
-        // when window width is >= 320px
-        650: {
-          slidesPerView: 2,
-          // spaceBetween: 30
+        650: {slidesPerView: 2,
         },
-        900: {
-            slidesPerView: 3,
-            // spaceBetween: 30
+        900: {slidesPerView: 3,
         },
       },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-    // autoplay: {
-    //     delay: 3000,
-    //   },
   });
-
 
 /* Initialize features swiper*/
 
 var menu = ['REGISTER', 'APPLY','RECIEVE'];
 
 var galerySwiper = new Swiper('.ft-swiper', {
-    spaceBetween: 100,
-    centeredSlides: true,
-    slidesOffsetAfter: 150,
-    loop: true,
-    
-    fadeEffect: {
-      crossFade: true,
-    },
-    effect: 'fade',
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<div data-aos-anchor-placement="top-bottom" class="features-pagination row '+className+'" data-aos="flip-left" data-aos-duration="1000"><div>'+(menu[index])+'</div></div>'
-        },
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
+  spaceBetween: 100,
+  centeredSlides: true,
+  slidesOffsetAfter: 150,
+  loop: true,
+  
+  fadeEffect: {
+    crossFade: true,
+  },
+  effect: 'fade',
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+          return '<div data-aos-anchor-placement="top-bottom" class="features-pagination row '+className+'" data-aos="flip-left" data-aos-duration="1000"><div>'+(menu[index])+'</div></div>'
+      },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 })
 
 /* Toggle subcription form in mobile */
-var formToggleButton = document.getElementById('sub-form-tgl');
-var subscriptionForm = document.getElementById('subscription-form');
-
-formToggleButton.addEventListener('click', function(){
-  // subscriptionForm.classList.toggle('closed');
+function toggleForm(){
   if (subscriptionForm.classList.contains('closed')) {
     subscriptionForm.classList.remove('closed');
     formToggleButton.innerHTML = '<h5>></h5>';
@@ -76,14 +76,26 @@ formToggleButton.addEventListener('click', function(){
     subscriptionForm.classList.add('closed');
     formToggleButton.innerHTML = '<h5><</h5>'
   }
+}
+
+formToggleButton.addEventListener('click', toggleForm)
+
+/* Toggle mobile menu in mobile */
+
+menuToggleButton.addEventListener('click', function(){
+  // subscriptionForm.classList.toggle('closed');
+  if (topMenu.classList.contains('open')) {
+    topMenu.classList.remove('open');
+    menuToggleButton.classList.remove('open');
+  }
+  else {
+    topMenu.classList.add('open');
+    menuToggleButton.classList.add('open');
+  }
 })
 
+
 /* form validation */
-
-var submitButtons = document.querySelectorAll('input[type="button"]');
-var textInputs = document.querySelectorAll('input[type="text"], input[type="tel"]');
-var radioInputs = document.querySelectorAll('input[type="radio"]');
-
 
 submitButtons.forEach(element => {
   element.addEventListener('click', submitForm)
@@ -136,8 +148,7 @@ function submitForm() {
     //clear form
     formInputs.forEach(element => {element.value=""});
     radioButtons.forEach(element => {element.checked=false});
-
-    //end
+    toggleForm();
     alert('Thanks');
   }
 };
